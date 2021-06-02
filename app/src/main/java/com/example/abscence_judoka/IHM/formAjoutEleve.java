b31b2a4da4eff9e43762ae1c2fcf41b142f6f4f6
bonneauTanguy <bonneau.tanguy1@gmail.com>
2021-06-02 18:38:05 +0200

    modification formulaire

--- app/src/main/java/com/example/abscence_judoka/IHM/formAjoutEleve.java
+++ app/src/main/java/com/example/abscence_judoka/IHM/formAjoutEleve.java
@@ -11,7 +11,7 @@ public class formAjoutEleve extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_ajouter);
+        setContentView(R.layout.activity_ajouter);
 
     }
 
