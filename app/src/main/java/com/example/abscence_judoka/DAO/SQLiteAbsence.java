1320029a850375aeac174e6bf3a62eb0b58ae056
bonneauTanguy <bonneau.tanguy1@gmail.com>
2021-06-02 16:12:09 +0200

    formulaire ajout élève

--- app/src/main/java/com/example/abscence_judoka/DAO/SQLiteAbsence.java
+++ app/src/main/java/com/example/abscence_judoka/DAO/SQLiteAbsence.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class SQLiteAbsence extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
     private static final String DATABASE_NAME = "GestionAbsences";
     private Context context=null;
 
