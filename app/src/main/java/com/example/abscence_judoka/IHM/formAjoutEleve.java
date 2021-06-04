f56a3f993ee3b5773fe609f32784f0ebe9da6eeb
bonneauTanguy <bonneau.tanguy1@gmail.com>
2021-06-04 17:05:52 +0200

    modif form ajout eleve. Ajout spinner categorie et ceinture

--- app/src/main/java/com/example/abscence_judoka/IHM/formAjoutEleve.java
+++ app/src/main/java/com/example/abscence_judoka/IHM/formAjoutEleve.java
@@ -5,14 +5,20 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.abscence_judoka.DAO.CategorieDAO;
+import com.example.abscence_judoka.DAO.CeintureDAO;
 import com.example.abscence_judoka.DAO.EleveDAO;
 import com.example.abscence_judoka.MainActivity;
+import com.example.abscence_judoka.Metier.Categorie;
+import com.example.abscence_judoka.Metier.Ceinture;
 import com.example.abscence_judoka.Metier.Eleve;
 import com.example.abscence_judoka.R;
 
@@ -24,6 +30,8 @@ public class formAjoutEleve extends AppCompatActivity {
     private EditText prenom;
     private EditText dateNaiss;
     private Button ajouter;
+    private Spinner categorie;
+    private Spinner ceinture;
     private SQLiteDatabase db;
     private boolean verifVide;
 
@@ -37,7 +45,31 @@ public class formAjoutEleve extends AppCompatActivity {
         nom = (EditText)findViewById(R.id.sNom);
         prenom = (EditText)findViewById(R.id.sPrenom);
         dateNaiss = (EditText)findViewById(R.id.sDateNaiss);
+        categorie = (Spinner)findViewById(R.id.listCeinture);
+        ceinture = (Spinner)findViewById(R.id.listCateg);
 
+        ArrayList<Categorie> listCateg = new ArrayList<Categorie>();
+        CategorieDAO bdd = new CategorieDAO(this);
+        bdd.open();
+        listCateg = bdd.read();
+        ArrayList<String> c = new ArrayList<>();
+        for (int i = 0; i < listCateg.size(); i++){
+            c.add(listCateg.get(i).getLibelleCategorie());
+        }
+        bdd.close();
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, c);
+        categorie.setAdapter(spinnerAdapter);
+
+        ArrayList<Ceinture> listCeint = new ArrayList<Ceinture>();
+        CeintureDAO bd = new CeintureDAO(this);
+        bd.open();
+        listCeint = bd.read();
+        ArrayList<String> a = new ArrayList<>();
+        for (int i = 0; i < listCeint.size(); i++){
+            a.add(listCeint.get(i).getLibelleCeinture());
+        }
+        bd.close();
+        ArrayAdapter<String> spinnerAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, a);
     }
 
     private View.OnClickListener retourListener = new View.OnClickListener() {
@@ -71,15 +103,23 @@ public class formAjoutEleve extends AppCompatActivity {
             bdd.insert(eleveAjout);
             Toast.makeText(formAjoutEleve.this,"élève ajouter", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(formAjoutEleve.this, "veuillez remplir tout les champs", Toast.LENGTH_LONG).show();
+            Toast.makeText(formAjoutEleve.this, "veuillez remplir tous les champs", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void viderChampsApresInsert(){
+        nom.getText().clear();
+        prenom.getText().clear();
+        dateNaiss.getText().clear();
     }
 
     private View.OnClickListener ajoutListener = new View.OnClickListener(){
 
         @Override
         public void onClick(View v) {
-
+            verifTextevide();
+            //insertionEleve();
+            viderChampsApresInsert();
         }
     };
 
